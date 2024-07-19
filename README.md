d8aa73a27e705705647710330f5bcd5bf6447d30
Seya <31957516+sglkc@users.noreply.github.com>
2024-07-19 22:49:19 +0700

    docs: add README.md, LICENSE
    feat: get help from README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+<pre>
+Request: Supports GET and POST, query and body, JSON and form body.
+Options:
+  text  (required)     Text to read
+  lang  (default: en)  Speaker language, reference: https://developers.google.com/admin-sdk/directory/v1/languages
+  speed (default: 1)   Audio speed, higher value is faster
+  pitch (default: 1)   Audio pitch, higher value is higher (yeah)
+Example:
+  (original female) <a href="/?text=こんにちは&lang=ja">/?text=こんにちは&lang=ja</a>
+  (male pitch) <a href="/?text=こんにちは&lang=ja&pitch=0.8">/?text=こんにちは&lang=ja&pitch=0.8</a>
+</pre>
--- index.js
+++ index.js
@@ -19,19 +19,7 @@ async function handler(req, res) {
 
   if (!text || !text.length) {
     res.header('content-type', 'text/html; charset=utf-8')
-    res.code(400).send(
-      `<pre>
-Request: Supports GET and POST, query and body, JSON and form body.
-Options:
-  text  (required)     Text to read
-  lang  (default: en)  Speaker language, reference: https://developers.google.com/admin-sdk/directory/v1/languages
-  speed (default: 1)   Audio speed, higher value is faster
-  pitch (default: 1)   Audio pitch, higher value is higher (yeah)
-Example:
-  (original female) <a href="/?text=こんにちは&lang=ja">/?text=こんにちは&lang=ja</a>
-  (male pitch) <a href="/?text=こんにちは&lang=ja&pitch=0.8">/?text=こんにちは&lang=ja&pitch=0.8</a>
-</pre>`
-    )
+    res.code(400).send(fs.readFileSync('README.md'))
 
     return res
   }
